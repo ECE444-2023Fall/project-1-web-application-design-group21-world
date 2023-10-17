e148792591a2290913e108a3212296769ede06bb
RishabhSaini <rishabhsaini01@gmail.com>
2023-10-16 23:04:59 -0400

    preliminary User API

--- main.py
+++ main.py
@@ -39,11 +39,36 @@ def index():
     
     return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), valid_email=session.get('valid_email'))
 
+@app.route("/users")
+def user_list():
+    users = db.session.execute(db.select(User).order_by(User.username)).scalars()
+    return users
 
-@app.route("/user/<name>")
-def user(name):
-    return render_template("user.html", name=name)
+@app.route("/users/create", methods=["GET", "POST"])
+def user_create():
+    if request.method == "POST":
+        user = User(
+            username=request.form["username"],
+            email=request.form["email"],
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("user_detail", id=user.id))
+return render_template("user/create.html")
 
+@app.route("/user/<int:id>")
+def user_detail(id):
+    user = db.get_or_404(User, id)
+return render_template("user/detail.html", user=user)
+
+@app.route("/user/<int:id>/delete", methods=["GET", "POST"])
+def user_delete(id):
+	user = db.get_or_404(User, id)
+    if request.method == "POST":
+        db.session.delete(user)
+        db.session.commit()
+        return redirect(url_for("user_list"))
+return render_template("user/delete.html", user=user)
 
 # if __name__ == '__main__':
 #     app.run()
