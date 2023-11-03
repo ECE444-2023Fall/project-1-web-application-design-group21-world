7f2143736c9a443d36cde4e2e8493bbcc9e30dc6
RishabhSaini <rishabhsaini01@gmail.com>
2023-11-03 01:08:09 -0400

    Fix the organizer test case duplication

--- tests/functional/test_organizer.py
+++ tests/functional/test_organizer.py
@@ -26,15 +26,3 @@ def test_organizer_insert_multiple(client):
          assert organizer is not None
          assert organizer.organizer_email == test_organizer["organizer_email"]
          assert organizer.organizer_name  == test_organizer["organizer_name"]
-
-def test_organizer_insert_multiple(client):
-    """Ensure that organizers can be added to Organizer Table"""
-    test_organizer = dict(organizer_name="test organizer 02", organizer_email="testorganizer02@gmail.com")
-
-    response = client.post("/organizer/create", json=test_organizer, follow_redirects=True)
-    assert response.status_code == 200
-    with client:
-         organizer = Organizer.query.filter_by(organizer_name=test_organizer["organizer_name"]).first()
-         assert organizer is not None
-         assert organizer.organizer_email == test_organizer["organizer_email"]
-         assert organizer.organizer_name  == test_organizer["organizer_name"]
