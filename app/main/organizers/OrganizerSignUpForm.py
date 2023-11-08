8a26fff76a38a25a1f04bea0c20ca6f00d567628
Ultare1717 <67229334+Ultare1717@users.noreply.github.com>
2023-11-08 17:13:04 -0400

    Added URL validation in Organizer creation form

--- app/main/organizers/OrganizerSignUpForm.py
+++ app/main/organizers/OrganizerSignUpForm.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, SelectField, StringField, SubmitField, validators
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, URL
 
 class OrganizerSignupForm(FlaskForm):
     organization_name = StringField("What is your organization name?", validators=[DataRequired()])
@@ -18,8 +18,8 @@ class OrganizerSignupForm(FlaskForm):
         validators=[DataRequired()],
     )
     organization_description = StringField("Organization Description", validators=[Length(max=500)])
-    organization_website_link = StringField("Website Link")
-    organization_instagram_link = StringField("Instagram Link")
+    organization_website_link = StringField("Website Link", validators=[URL()])
+    organization_instagram_link = StringField("Instagram Link", validators=[URL()])
     organization_linkedin_link = StringField("LinkedIn Link")
 
     submit = SubmitField("Submit")
