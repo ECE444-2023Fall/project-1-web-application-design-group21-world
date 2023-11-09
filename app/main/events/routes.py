f12d420bfb3a58a169b4b38334bd7fb300c0f5de
arshar2411 <arjunmsharm@gmail.com>
2023-11-09 11:10:03 -0500

    testing hooks

--- app/main/events/routes.py
+++ app/main/events/routes.py
@@ -1,8 +1,9 @@
-from flask import current_app, flash, redirect, render_template, request, session, url_for
+from flask import render_template, request, session
 
 from ... import db
 from ...models import Event
 from . import events_blueprint
+
 
 @events_blueprint.route("/events/create", methods=["GET", "POST"])
 def events_create():
@@ -18,18 +19,22 @@ def events_create():
             google_map_link=request.json["google_map_link"],
             fee=request.json["fee"],
             has_rsvp=request.json["has_rsvp"],
-            external_registration_link=request.json["external_registration_link"]
+            external_registration_link=request.json["external_registration_link"],
         )
         db.session.add(event)
         db.session.commit()
-        #print("URL FOUND", url_for("event.event_list"))
-        #return redirect(url_for("event.event_list"))
+        # print("URL FOUND", url_for("event.event_list"))
+        # return redirect(url_for("event.event_list"))
         return render_template("events_new.html", name=event.event_name, event=event)
     return render_template("index_event.html")
 
 
-#@events_blueprint.route("/event/list", methods=["GET"])
-#def events_list():
-#    event = Event.query.all()
-#    if event is not None:
-#        return render_template("events_new.html", name=session.get("first_name", "Stranger"), event=event)
+@events_blueprint.route("/events/list", methods=["GET"])
+def events_list():
+    events_list = Event.query.all()
+    if events_list is not None:
+        return render_template(
+            "events.html",
+            name=session.get("first_name", "Stranger"),
+            events_list=events_list,
+        )
