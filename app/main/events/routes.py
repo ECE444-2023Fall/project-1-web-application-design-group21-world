2fc253450bbf55b9f22bd6257d790fa799fdc056
arshar2411 <arjunmsharm@gmail.com>
2023-11-09 11:10:03 -0500

    testing hooks

--- app/main/events/routes.py
+++ app/main/events/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, session
 
 from ... import db
 from ...models import Event
@@ -29,16 +29,16 @@ def events_create():
         db.session.commit()
         # print("URL FOUND", url_for("event.event_list"))
         # return redirect(url_for("event.event_list"))
+        return render_template("events_new.html", name=event.event_name, event=event)
+    return render_template("index_event.html")
+
+
+@events_blueprint.route("/events/list", methods=["GET"])
+def events_list():
+    events_list = Event.query.all()
+    if events_list is not None:
         return render_template(
-            "events_new.html", name=event.event_name, event=event
+            "events.html",
+            name=session.get("first_name", "Stranger"),
+            events_list=events_list,
         )
-
-
-# @events_blueprint.route("/events/list", methods=["GET"])
-# def events_list():
-#     events_list = Event.query.all()
-#     if events_list is not None:
-#         return render_template(
-#             "events_list.html",
-#             events_list=events_list,
-#         )
