a83e18e0ca107b00e4c1dc7716fb7f5406f29dc8
Breno <breno.pinho1@gmail.com>
2023-11-10 15:08:43 -0500

    added new fonts for use on whole website. Fixed some typos and fixed issues on text from myEvents and eventDetails pages.

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,30 +4,30 @@ from wtforms.validators import DataRequired, Email
 
 
 class UserSignUpForm(FlaskForm):
-    name = StringField("What is your full name?", validators=[DataRequired()])
-    email = EmailField("What is your UofT email address?", validators=[DataRequired(), Email()])
+    name = StringField("What is your Full Name?", validators=[DataRequired()])
+    email = EmailField("What is your UofT Email Address?", validators=[DataRequired(), Email()])
     password = PasswordField(
-        "Enter your password",
+        "Enter your Password (*)",
         validators=[
             DataRequired(),
             validators.Length(min=8, max=80),
             validators.EqualTo("confirm", message="Passwords must match"),
         ],
     )
-    confirm = PasswordField("Repeat passoword")
+    confirm = PasswordField("Repeat Password (*)")
     faculty = SelectField(
-        "Faculty",
+        "Faculty (*)",
         choices=[("Commerce", "Rotman"), ("Engineering", "Eng"), ("ArtSci", "A&S")],
         validators=[DataRequired()],
     )
-    major = StringField("Major", validators=[DataRequired()])
+    major = StringField("Major (*)", validators=[DataRequired()])
     campus = SelectField(
-        "Campus",
+        "Campus (*)",
         choices=[("St. George"), ("Scarborough"), ("Missasauga")],
         validators=[DataRequired()],
     )
     year_of_study = SelectField(
-        "Year of Study",
+        "Year of Study (*)",
         choices=[("1st"), ("2nd"), ("3rd"), ("4th"), ("5th"), ("Masters"), ("PhD"), ("other")],
         validators=[DataRequired()],
     )
@@ -35,8 +35,8 @@ class UserSignUpForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = EmailField("What is your UofT email address?", validators=[DataRequired(), Email()])
-    password = PasswordField("Enter your password", validators=[DataRequired()])
+    email = EmailField("What is your UofT Email Address?", validators=[DataRequired(), Email()])
+    password = PasswordField("Enter your Password", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 class userSignupInterestForm(FlaskForm):
--- app/main/organizers/OrganizerSignUpForm.py
+++ app/main/organizers/OrganizerSignUpForm.py
@@ -1,27 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, SelectField, StringField, SubmitField, FileField, validators
 from flask_wtf.file import FileAllowed
-from wtforms.validators import DataRequired, Email, Length, URL
+from wtforms.validators import DataRequired, Email, Length, URL, Optional
 
 class OrganizerSignupForm(FlaskForm):
-    organization_name = StringField("What is your organization name?", validators=[DataRequired()])
-    organization_email = EmailField("What is your organization UofT email address?", validators=[DataRequired(), Email()])
+    organization_name = StringField("What is your Organization Name (*)?", validators=[DataRequired()])
+    organization_email = EmailField("What is your Organization UofT Email Address? (*)", validators=[DataRequired(), Email()])
     password = PasswordField(
-        "Enter your password",
+        "Enter your password (*)",
         validators=[
             DataRequired(),
             validators.Length(min=8, max=80),
+            validators.EqualTo("confirm", message="Passwords must match"),
         ],
     )
+    confirm = PasswordField("Repeat Password (*)")
     organization_campus = SelectField(
         "Campus",
-        choices=[("UTSG"), ("UTSC"), ("UTM")],
+        choices=[("St. George"), ("Scarborough"), ("Missasauga")],
         validators=[DataRequired()],
     )
-    image = FileField("Upload Organizer Image", validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif'])])
-    organization_description = StringField("Organization Description", validators=[Length(max=500)])
-    organization_website_link = StringField("Website Link", validators=[URL()])
-    organization_instagram_link = StringField("Instagram Link", validators=[URL()])
-    organization_linkedin_link = StringField("LinkedIn Link")
+    image = FileField("Upload Organizer Image (*)", validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif'])])
+    organization_description = StringField("Organization Description (*)", validators=[Length(max=500)])
+    organization_website_link = StringField("Website Link", validators=[URL(), Optional()])
+    organization_instagram_link = StringField("Instagram Link", validators=[URL(), Optional()])
+    organization_instagram_link = StringField("Instagram Link", validators=[URL(), Optional()])
 
     submit = SubmitField("Submit")
