1b2c50071e7fb93f7f6846c1816ace7dfc62d386
arshar2411 <arjunmsharm@gmail.com>
2023-11-11 18:14:48 -0500

    path created

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config:
     # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     # FLASKY_MAIL_SUBJECT_PREFIX = "[Flasky]"
     # FLASKY_MAIL_SENDER = "Flasky Admin <flasky@example.com>"
+    IMAGE_PATH = os.environ.get("IMAGE_PATH") or "app/static/assets"
     FLASKY_ADMIN = os.environ.get("FLASKY_ADMIN")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
